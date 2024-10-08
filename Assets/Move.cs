e787b25a232dd7885856f8982b8b5350c65d3792
OuiamBak <oubakhouche@osullivan-quebec.qc.ca>
2024-10-08 15:52:03 -0400

    design 2
    from school

---
+++ Assets/Move.cs
@@ -0,0 +1,44 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Move : MonoBehaviour
+{
+    [SerializeField] private Rigidbody2D _rb;
+    [SerializeField] private float _acceleration = 5f;
+    [SerializeField] private float _maxSpeed = 10f;
+    [SerializeField] private float _speed = 0f;
+
+    // Update is called once per frame
+    void Update()
+    {
+        // Avancer vers la droite quand on appuie sur la flèche droite
+        if (Input.GetKey(KeyCode.RightArrow))
+        {
+            _speed += _acceleration * Time.deltaTime;
+            if (_speed >= _maxSpeed)
+            {
+                _speed = _maxSpeed;
+            }
+            _rb.velocity = new Vector2(_speed, _rb.velocity.y);
+        }
+
+        // Avancer vers la gauche quand on appuie sur la flèche gauche
+        if (Input.GetKey(KeyCode.LeftArrow))
+        {
+            _speed -= _acceleration * Time.deltaTime;
+            if (_speed <= -_maxSpeed)
+            {
+                _speed = -_maxSpeed;
+            }
+            _rb.velocity = new Vector2(_speed, _rb.velocity.y);
+        }
+
+        // Si aucune touche n'est pressée, ralentir progressivement
+        if (!Input.GetKey(KeyCode.RightArrow) && !Input.GetKey(KeyCode.LeftArrow))
+        {
+            _speed = Mathf.MoveTowards(_speed, 0, _acceleration * Time.deltaTime);
+            _rb.velocity = new Vector2(_speed, _rb.velocity.y);
+        }
+    }
+}
